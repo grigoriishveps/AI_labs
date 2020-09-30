52960418fe429141bf0682736394defe9e6d542e
gdesyatnikov <gdesyatnikov>
2020-09-30 16:30:18 +0300

    Создана архитектура

--- 1lab/dfs.py
+++ 1lab/dfs.py
@@ -1,18 +1,79 @@
 import numpy as np
 from State import State
-import sys
+
 def start_dfs(start, answer):
-    sys.setrecursionlimit(150000)
+
     history = [(start,"start")]
     print(start)
-    dfs(start, answer, history)
+    #dfs(start, answer, history)
+    dfs_two(start, answer, history)
     print("Длина истории" ,len(history))
+
+
     x = 0
     while(x!=-1):
         print(history[x][0])
         print("done - ", history[x][1])
         x = int(input())
     print("Конец просмотра")
+
+def dfs_two(state, answer, history):
+
+    stack = [(state, 0)]
+
+    while len(stack)!=0:
+        state = stack[-1][0]
+        done = stack[-1][1]
+        #flag = False
+        if len(stack) %1000==0:
+            print(len(stack))
+        for i in range(len(stack)-2, 0,-1):
+
+            if stack[i][0] == state:
+                history.append((state, "repeat"))
+                stack.pop()
+                break
+        # if flag:
+        #     continue
+        else:
+            if state==answer or len(history)==10001:
+                history.append((state, "end"))
+
+                return True
+            else:
+                if done == 0 and state.has_right():
+                    new_state = state.move_right()
+                    history.append((new_state, "right"))
+                    stack.pop()
+                    stack.append((state, 1))
+                    stack.append((new_state, 0))
+                elif done <= 1 and state.has_down():
+                    new_state = state.move_down()
+                    history.append((new_state, "down"))
+                    stack.pop()
+                    stack.append((state, 2))
+                    stack.append((new_state, 0))
+                elif done <= 2 and state.has_left():
+                    new_state = state.move_left()
+                    history.append((new_state, "left"))
+                    stack.pop()
+                    stack.append((state, 3))
+                    stack.append((new_state, 0))
+                elif done <= 3 and state.has_up():
+                    new_state = state.move_up()
+                    history.append((new_state, "up"))
+                    stack.pop()
+                    stack.append((state, 4))
+                    stack.append((new_state, 0))
+                else:
+                    stack.pop()
+                    history.append((state, "block -1"))
+
+                #print("Ошибка")
+                #raise Exception("афваывываывпваьброд")
+
+
+
 
 def dfs(state, answer, history):
     for i in range(len(history)-2, 0,-1):
@@ -21,7 +82,7 @@ def dfs(state, answer, history):
         elif history[i][0] == state:
             history.append((state, "repeat"))
             return False;
-    if state==answer or len(history)==100001:
+    if state==answer or len(history)==10001:
         history.append((state, "end"))
         return True
     else:
@@ -43,5 +104,6 @@ def dfs(state, answer, history):
                 return True
 
         history.append((state, "block -1"))
+        return False
         #print("Ошибка")
         #raise Exception("афваывываывпваьброд")
