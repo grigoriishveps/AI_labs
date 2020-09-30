42564732b3e606581d7ca46bc4151d2f705a1f51
Nick <sshedl22@gmail.com>
2020-09-30 21:01:01 +0300

    WIP. Added multithreading but not work quit good

--- 1lab/bfs.py
+++ 1lab/bfs.py
@@ -2,51 +2,75 @@ from typing import List
 from State import State
 import numpy as np
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from queue import Queue
+import time
 
 global_states = list()
-
+q = Queue()
 i = 0
 
+done = False
+final_state = State(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]))
+state_hist = list()
 
-def blind_search_bfs(stack: List[State]):
-    final_state = State(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]))
-    state_hist = list()
-    i = 0
-    while len(stack) != 0:
-        i += 1
-        state = stack.pop(0)
-        if state == final_state:
-            print("Iterations: ", i)
-            return state
-        else:
-            if state in state_hist:
-                continue
-            else:
-                state_hist.append(state)
-            if state.has_up():
-                stack.append(state.move_up())
-                # print("Empty moved up")
-                # print(stack[-1])
-            if state.has_down():
-                stack.append(state.move_down())
-                # print("Empty moved down")
-                # print(stack[-1])
-            if state.has_left():
-                stack.append(state.move_left())
-                # print("Empty moved left")
-                # print(stack[-1])
-            if state.has_right():
-                stack.append(state.move_right())
-                # print("Empty moved right")
-                # print(stack[-1])
-        if len(stack) % 1000 == 0:
+
+def blind_search_bfs():
+    with ThreadPoolExecutor() as executor:
+        global done
+        global q
+        while not done:
+            executor.submit(thread_func).add_done_callback(work_done)
+            # if queue.qsize() % 1000 == 0:
             print("Stack")
-            print(len(stack))
-        if len(state_hist) % 1000 == 0:
-            print("State hist")
-            print(len(state_hist))
+            print(q.qsize())
+            # if len(state_hist) % 1000 == 0:
+            #     print("State hist")
+            #     print(len(state_hist))
+
+
+def work_done(future):
+    t = future.result()
+    if t is not None:
+        global done
+        done = True
+        print(t)
+
+
+def thread_func():
+    global q
+    state = q.get()
+    if state == final_state:
+        global done
+        if not done:
+            return state
+    else:
+        global state_hist
+        if state in state_hist:
+            return
+        else:
+            state_hist.append(state)
+        if state.has_up():
+            q.put(state.move_up())
+            # print("Empty moved up")
+            # print(stack[-1])
+        if state.has_down():
+            q.put(state.move_down())
+            # print("Empty moved down")
+            # print(stack[-1])
+        if state.has_left():
+            q.put(state.move_left())
+            # print("Empty moved left")
+            # print(stack[-1])
+        if state.has_right():
+            q.put(state.move_right())
+            # print("Empty moved right")
+            # print(stack[-1])
 
 
 if __name__ == '__main__':
-    res = blind_search_bfs([State(np.array([[5, 8, 3], [4, 0, 2], [7, 6, 1]]))])
-    print(res)
+    time1 = time.perf_counter()
+    q.put(State(np.array([[1, 2, 3], [4, 5, 0], [6, 7, 8]])))
+    blind_search_bfs()
+    time2 = time.perf_counter()
+    print(time2 - time1)
