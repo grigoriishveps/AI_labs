995480a3fc466afc3f7d0994afe72eea5ddd03c8
gdesyatnikov <!5ww=v^c!LIYR+26!2PMsMUZ>
2020-10-04 19:41:23 +0300

    ready dfs

--- 1lab/dfs.py
+++ 1lab/dfs.py
@@ -1,15 +1,14 @@
-import numpy as np
-from State import State
 
-def start_dfs(start, answer):
-
+def start_dfs(start, answer, with_pause):
+    f = open('log.txt', 'w')
     history = [(start,"start")]
     print(start)
-    #dfs(start, answer, history)
-    dfs_two(start, answer, history)
+    dfs_two(start, answer, history, with_pause)
     print("Длина истории" ,len(history))
-
-
+    for i in range(len(history)):
+        f.write(str(i) + '\n')
+        f.write(str(history[i][0]) + '\n')
+        f.write("done - " + str(history[i][1]) + '\n')
     x = 0
     while(x!=-1):
         print(history[x][0])
@@ -17,93 +16,46 @@ def start_dfs(start, answer):
         x = int(input())
     print("Конец просмотра")
 
-def dfs_two(state, answer, history):
-
-    stack = [(state, 0)]
-
+def dfs_two(state, answer, history, with_pause):
+    stack = [[state, 0, 0]]
     while len(stack)!=0:
         state = stack[-1][0]
         done = stack[-1][1]
-        #flag = False
-        if len(stack) %1000==0:
-            print(len(stack))
-        for i in range(len(stack)-2, 0,-1):
+        pred = stack[-1][2]
 
+        if state==answer:
+            history.append((state, "end"))
+            return True
+        # проверка повтора узла
+        for i in range(len(stack)-2, -1,-1):
             if stack[i][0] == state:
                 history.append((state, "repeat"))
                 stack.pop()
                 break
-        # if flag:
-        #     continue
         else:
-            if state==answer or len(history)==10001:
-                history.append((state, "end"))
-
-                return True
+            # Выбор следующего узла
+            if pred != 3 and done == 0 and state.has_right():
+                new_state = state.move_right()
+                history.append((new_state, "right"))
+                stack[-1][1]= 1
+                stack.append([new_state, 0, 1])
+            elif pred != 4 and done <= 1 and state.has_down():
+                new_state = state.move_down()
+                history.append((new_state, "down"))
+                stack[-1][1]= 2
+                stack.append([new_state, 0, 2])
+            elif pred != 1 and done <= 2 and state.has_left():
+                new_state = state.move_left()
+                history.append((new_state, "left"))
+                stack[-1][1]= 3
+                stack.append([new_state, 0, 3])
+            elif pred != 2 and done <= 3 and state.has_up():
+                new_state = state.move_up()
+                history.append((new_state, "up"))
+                stack[-1][1] = 4
+                stack.append([new_state, 0, 4])
             else:
-                if done == 0 and state.has_right():
-                    new_state = state.move_right()
-                    history.append((new_state, "right"))
-                    stack.pop()
-                    stack.append((state, 1))
-                    stack.append((new_state, 0))
-                elif done <= 1 and state.has_down():
-                    new_state = state.move_down()
-                    history.append((new_state, "down"))
-                    stack.pop()
-                    stack.append((state, 2))
-                    stack.append((new_state, 0))
-                elif done <= 2 and state.has_left():
-                    new_state = state.move_left()
-                    history.append((new_state, "left"))
-                    stack.pop()
-                    stack.append((state, 3))
-                    stack.append((new_state, 0))
-                elif done <= 3 and state.has_up():
-                    new_state = state.move_up()
-                    history.append((new_state, "up"))
-                    stack.pop()
-                    stack.append((state, 4))
-                    stack.append((new_state, 0))
-                else:
-                    stack.pop()
-                    history.append((state, "block -1"))
-
-                #print("Ошибка")
-                #raise Exception("афваывываывпваьброд")
+                stack.pop()
+                history.append((state, "block -1"))
 
 
-
-
-def dfs(state, answer, history):
-    for i in range(len(history)-2, 0,-1):
-        if history[i][1]=='start' or history[i][1]=="block -1":
-            break
-        elif history[i][0] == state:
-            history.append((state, "repeat"))
-            return False;
-    if state==answer or len(history)==10001:
-        history.append((state, "end"))
-        return True
-    else:
-        if state.has_right():
-            history.append((state.move_right(), "right"))
-            if dfs(state.move_right(), answer, history):
-                return True
-        if state.has_down():
-            history.append((state.move_down(), "down"))
-            if dfs(state.move_down(), answer, history):
-                return True
-        if state.has_left():
-            history.append((state.move_left(), "left"))
-            if dfs(state.move_left(), answer, history):
-                return True
-        if state.has_up():
-            history.append((state.move_up(), "up"))
-            if dfs(state.move_up(), answer, history):
-                return True
-
-        history.append((state, "block -1"))
-        return False
-        #print("Ошибка")
-        #raise Exception("афваывываывпваьброд")
