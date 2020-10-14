70d34ccb7686f4cc98604184a17405564ba46c51
gdesyatnikov <!5ww=v^c!LIYR+26!2PMsMUZ>
2020-10-14 15:24:50 +0300

    good dfs done)))

--- 1lab/dfs.py
+++ 1lab/dfs.py
@@ -6,6 +6,11 @@ def check_state(hsh_tbl, state):
     else:
         return True
 
+def state_list(stack ,is_last):
+    result_arr = []
+    for i in (stack[-3:] if is_last else stack[:3]):
+        result_arr.append(i[0])
+    return result_arr
 
 def start_dfs(start, answer, with_pause):
     f = open('log.txt', 'w')
@@ -73,49 +78,49 @@ def dfs_without_pause(state, answer, history):
 def dfs_with_pause(state, answer, history):
     stack = [[state, 0, 0]]
     hsh_tbl = {}
-    print(
-        "Для просмотра 5последних значений стека на каждом этапе алгоритма введите строку stack, иначе простот нажмите Enter")
+    duplicate_vertices = list()
     while len(stack) != 0:
+        flag_pop = False
         state = stack[-1][0]
         done = stack[-1][1]
         pred = stack[-1][2]
-
-        print("Текущее обрабатываемое состояние")
-        print(state)
-        x = input()
-        if (x == "stack"):
-            print("Последние 5 значения стека")
-            for i in range(len(stack) - 1, max(-1, len(stack) - 6), -1):
-                print(stack[i][0])
 
         if state == answer:
             return True
         # проверка повтора узла
         if done == 0 and check_state(hsh_tbl, state):
-            print("done - repeat")
+            duplicate_vertices.append(state)
             stack.pop()
         else:
             # Выбор следующего узла
             if pred != 3 and done == 0 and state.has_right():
                 new_state = state.move_right()
-                print("done - right\n")
                 stack[-1][1] = 1
                 stack.append([new_state, 0, 1])
             elif pred != 4 and done <= 1 and state.has_down():
                 new_state = state.move_down()
-                print("done - down\n")
                 stack[-1][1] = 2
                 stack.append([new_state, 0, 2])
             elif pred != 1 and done <= 2 and state.has_left():
                 new_state = state.move_left()
-                print("done - left\n")
                 stack[-1][1] = 3
                 stack.append([new_state, 0, 3])
             elif pred != 2 and done <= 3 and state.has_up():
                 new_state = state.move_up()
-                print("done - up\n")
                 stack[-1][1] = 4
                 stack.append([new_state, 0, 4])
             else:
                 stack.pop()
-                print("done - block -1\n")
+                flag_pop = True
+
+        print("--------------------------------------------------------------------------------------")
+        print(f"Current state: \n {state}")
+        print("Duplicated vertices added from last step:", *duplicate_vertices, sep='\n')
+        print("Current state of queue first 3:", *state_list(stack, False), "and last 3 objects:", *state_list(stack, True), sep='\n')
+        if not (flag_pop):
+            print("Added to queue vertice on current step:", stack[-1][0], sep='\n')
+        else:
+            print("On step vertice was delete from stack beacuse all direction was found or it was dublicate")
+        print("--------------------------------------------------------------------------------------")
+        print("Press any key to move to next step")
+        input()
